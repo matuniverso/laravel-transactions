4e11d8defd23e8f041c1de055b616a75ea25cf98
eumatv <miliandroxd@gmail.com>
2022-01-05 00:08:33 -0300

    add sanctum token route for authentication

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,29 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\TransactionController;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required'],
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->email)->plainTextToken;
+});
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/account', AccountController::class)->name('account');
