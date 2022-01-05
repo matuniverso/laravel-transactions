ac1a957f47dc51e524758463b8e01c9613411e39
eumatv <miliandroxd@gmail.com>
2022-01-05 00:13:03 -0300

    registering user observer in service provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,36 +2,22 @@
 
 namespace App\Providers;
 
-use App\Events\UserRegistered;
-use App\Listeners\CreateAccountForUser;
+use App\Models\User;
+use App\Observers\UserObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array
-     */
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
-        ],
-        UserRegistered::class => [
-            CreateAccountForUser::class
         ]
     ];
 
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        User::observe(UserObserver::class);
     }
 }
